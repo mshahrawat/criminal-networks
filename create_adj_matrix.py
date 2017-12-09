d22f879774e027842656ebd29be7182f5bd6e62f
Malika Shahrawat <malikas@mit.edu>
2017-12-09 01:13:32 -0500

    can create routes with edges from basic adj matrix of regions

--- create_adj_matrix.py
+++ create_adj_matrix.py
@@ -6,21 +6,13 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 cooffending_df = pd.read_csv('Cooffending.csv', na_values=['.'])
-# number of unique crimes per municipality 
-# print cooffending_df.groupby('MUN')['SeqE'].nunique()
+num_unique_crimes_per_mun = cooffending_df.groupby('MUN')['SeqE'].nunique()
 num_mun = cooffending_df['MUN'].nunique()
 
 mun_df = pd.read_csv('mun_clean.csv')
-num_municipalities = mun_df['municipal_code'].nunique()
-num_regions = mun_df['admin_region'].nunique()
-
-# mun_codes_df = pd.read_csv('mun_codes.csv')
-# num_mun_codes = mun_codes_df['municipal_code'].nunique()
-# directed graph with edges both ways
+mun_codes_df = pd.read_csv('mun_codes.csv')
 
 mun_df['admin_region'] = mun_df['admin_region'].apply(lambda x: int(x[-3:-1]))
-
-# cooffending_df = cooffending_df.head(10)
 cooffending_df = cooffending_df.loc[cooffending_df['annee'] == 2003]
 mun_dict = dict(zip(mun_df.municipal_code,mun_df.admin_region))
 cooffending_df['region'] = cooffending_df['MUN'].map(lambda x: mun_dict.get(x, np.NaN))
@@ -28,19 +20,22 @@ cooffending_df = cooffending_df.dropna()
 cooffending_df['region'] = cooffending_df['region'].astype(int)
 cooffending_df.to_csv('cooffending_regions.csv')
 
+cooffending_df = pd.read_csv('cooffending_regions.csv')
+
+region_adj_matrix = np.genfromtxt('region_adj.csv', delimiter=',')
 
 '''
 TODO: create graph G for with edges for neighboring municipalities
 edges will have cost associated with them as specified
 '''
-# adj_list = []
-# G = nx.Graph()
-# G.add_edges_from(adj_list)
 
 # create dummy graph for testing
+# NEED directed graph with edges both ways
 # G = nx.complete_graph(4)
-# source = 0
-# target = 3
+# note that graph is 0 indexed but the regions are from 1
+source = 3
+target = 8
+G = nx.from_numpy_matrix(region_adj_matrix)
 
 def get_route_edge_adj_matrix(G, source, target, cutoff=None):
 	edges = list(G.edges())
@@ -64,4 +59,4 @@ def get_route_edge_adj_matrix(G, source, target, cutoff=None):
 
 	return adj_matrix
 
-# print get_route_edge_adj_matrix(G, source, target)
+print get_route_edge_adj_matrix(G, source, target)
